6aba5121b06d4527b309489d2d47c64bc987118b
AyuTUL <165144922+AyuTUL@users.noreply.github.com>
2025-06-29 15:58:05 +0545

    Add files via upload

--- DBMS/Notes/2lab.sql
+++ DBMS/Notes/2lab.sql
@@ -10,4 +10,7 @@ SELECT * FROM employee WHERE Esalary>30000;
 
 SELECT * FROM employee WHERE Esalary<25000 AND Eaddress='Dharan';
 
+SELECT * FROM employee WHERE Esalary IN(7000.0,4000.0,6000.0);
+SELECT * FROM employee WHERE Esalary NOT IN(7000.,4000.0,6000.0,4500.0,6900.0,53000.0,65000.0);
+
 SELECT Eid,Fname,Esalary,Edetails FROM employee WHERE Edetails='Manager';
