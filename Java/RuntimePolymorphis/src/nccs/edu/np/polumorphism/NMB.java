25b903d09c5e8296105af215f73417f0bacb343e
AyuTUL <ayushtuladhar007@gmail.com>
2025-07-02 21:48:18 +0545

    popo

---
+++ Java/RuntimePolymorphis/src/nccs/edu/np/polumorphism/NMB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nccs.edu.np.polumorphism;
+
+/**
+ *
+ * @author user
+ */
+public class NMB {
+    
+}
