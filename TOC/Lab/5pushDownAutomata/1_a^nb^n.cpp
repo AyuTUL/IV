b6280670d4cdcf45079120f79b7e394c7cee2994
AyuTUL <165144922+AyuTUL@users.noreply.github.com>
2025-08-29 14:27:18 +0545

    Add files via upload

--- TOC/Lab/5pushDownAutomata/1_a^nb^n.cpp
+++ TOC/Lab/5pushDownAutomata/1_a^nb^n.cpp
@@ -1,10 +1,11 @@
 //Lab 5.1: WAP to design PDA that accepts L={a^n b^n | n>=1}. Also check for aaaabbbb
-//incomplete
 #include <iostream>
 #include <stack>
 #include <string>
 using namespace std;
-bool pda(string& input) {
+bool pda(string& input) 
+{
+	char state='A';
     stack<char> s;
     s.push('z');
     for(int i=0;i<input.length();i++)
@@ -12,12 +13,7 @@ bool pda(string& input) {
 		switch(state)
 		{
 			case 'A':
-				if(input[i]=='a' && s.top()=='z')
-				{
-					s.push('a');
-					state='A';	
-				}
-				else if(input[i]=='a' && s.top()=='a')
+				if(input[i]=='a' && (s.top()=='z' || s.top()=='a'))
 				{
 					s.push('a');
 					state='A';	
@@ -27,6 +23,8 @@ bool pda(string& input) {
 					s.pop();
 					state='B';	
 				}
+				else
+					return false;
 				break;
 			case 'B':
 				if(input[i]=='b' && s.top()=='a')
@@ -34,40 +32,15 @@ bool pda(string& input) {
 					s.pop();
 					state='B';	
 				}
-				else if(input[i]=='\0' && s.top()=='z')
-				{
-					state='A';	
-				}
-				break;
-			case 'C':
-				if(input[i]=='0') state='D';
-				break;
-			case 'D':
-				if(input[i]=='1') state='E';
+				else 
+					return false;	
 				break;
 			}
 	}
-    int i=0,n=input.size();
-    while(i<n && inpout[i]=='a') {
-        stk.push('a');
-        i++;
-    }
-    if (i == 0) return false;
-
-    // State q1: read 'b's and pop 'A's from stack
-    while (i < n && input[i] == 'b') {
-        if (stk.empty() || stk.top() != 'A') return false;
-        stk.pop();
-        i++;
-    }
-
-    // At the end, input consumed, stack should have only 'Z' left
-    if (i == n && stk.size() == 1 && stk.top() == 'Z')
-        return true;
-
-    return false;
+	if(state=='B' && s.top()=='z')
+		state='C';
+	return(state=='C' && s.top()=='z');
 }
-
 int main() {
     string input;
     cout<<"Enter input : ";
@@ -79,12 +52,6 @@ int main() {
 			return 0;
 		}
     if(pda(input))
-        cout<<"PDA accepts the string : "<<input;
-    else
-        cout<<"PDA rejects the string : "<<input;
-    string test="aaaabbbb";
-    cout<<"\nTesting string:"<<test<<endl;
-    if (pda_accepts(test_str))
         cout<<"PDA accepts the string : "<<input;
     else
         cout<<"PDA rejects the string : "<<input;
